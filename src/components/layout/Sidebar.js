3d02b8d3010ad2c84322925b4df8410d6ee1b1a1
nzaih1999 <nzaih18@gmail.com>
2022-02-25 17:21:24 +0300

    shidaa

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -7,11 +7,10 @@ FaAd,
 } from 'react-icons/fa'
 export const Sidebar = () => ( 
 <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar-generic">
-        <li><span><FaInbox/></span><span>inbox</span></li>
-        <li><span><FaAd/></span><span>Today</span></li>
-      <li> <span><FaAccessibleIcon/></span><span>Tommorow</span></li>
-      <li> <span><FaAccessibleIcon/></span><span>Next Week</span></li>
+    <ul className="sidebar__generic">
+        <li data-testid="inbox" className="inbox"><span><FaInbox/></span><span> Inbox</span></li>
+        <li data-testid="today" className="today"><span><FaAd/></span><span>Today</span></li>
+      <li data-testid="next_7" className="next_7"> <span><FaAccessibleIcon/></span><span>Next Week</span></li>
     </ul>
     <div className="sidebar__middle">
         <span><FaChevronDown/></span>
